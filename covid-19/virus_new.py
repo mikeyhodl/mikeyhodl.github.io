a317998598b2b739c1af0c757d395a8971f9d455
Mike Owino <mike.owino40@gmail.com>
2020-08-31 18:40:44 +0300

    https://github.com/MikeOwino/MikeOwino.github.io.git

--- covid-19/virus_new.py
+++ covid-19/virus_new.py
@@ -10,8 +10,8 @@ import requests
 
 # browser = webdriver.Chrome("/Users/FengyuXu/Desktop/web_crawler/twitter_crawler/chromedriver")
 # browser = webdriver.Chrome("C:/workspace/chromedriver.exe")
-# browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
-browser = webdriver.Chrome("/Users/stevenbao/dev/chromedriver")
+browser = webdriver.Chrome("E:/dev/workspaces/chromedriver.exe")
+# browser = webdriver.Chrome("/Users/stevenbao/dev/chromedriver")
 
 # Variable Preparation
 now = str(datetime.now())
@@ -134,8 +134,8 @@ for item in items[2:]:
         name = "sao tome and principe"
     '''
 
-    confirmed = item.find_all("td")[0].text.split("\n")[0].replace(",", "").replace("No data", "0")
-    death = item.find_all("td")[1].text.split("\n")[0].replace(",", "").replace("No data", "0")
+    confirmed = item.find_all("td")[0].text.split("\n")[0].replace(",", "").replace("No data", "0").replace(" ", "")
+    death = item.find_all("td")[1].text.split("\n")[0].replace(",", "").replace("No data", "0").replace("[am]","")
     recovered = item.find_all("td")[2].text.split("\n")[0].replace(",", "").replace("No data", "0")
     if recovered == "–" or recovered == "—":
         recovered = "0"
