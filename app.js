ea71ce11730b024f4454d1bed6626cb13eec17d7
Mike Owino <mike.owino40@gmail.com>
2020-12-16 19:28:26 +0300

    koooo

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-const examplePromise = () => {
-  return new Promise((resolve, reject) => {
-    if (true) {
-      setTimeout( () => resolve('success'), 3000);
-    } else {
-      setTimeout( () => resolve('failed'), 5000);
-    }
-  });
-};
+const asyncHello = new Promise((resolve, reject) => { 
+  setTimeout(resolve, 1000, 'Hello!'); 
+}); 
+
+console.log(typeof asyncHello);
